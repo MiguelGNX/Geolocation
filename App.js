e663270b69a4cc2d07eb87dc4148ed5bca8211c6
MiguelGNX <mmartines376@accitesz.com>
2023-04-27 11:12:45 -0600

    Primer Repositorio

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'; 
+
 
 export default function App() {
+    const [origin,setOrigin] = React.useState({
+      latitude:  20.07607038194086,  
+      longitude: -102.27207159182097,
+    })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style = {styles.map}
+      initialRegion={{
+        latitude: origin.latitude,
+        longitude: origin.longitude,
+        latitudeDelta: 0.09,
+        longitudeDelta: 0.09
+      }}
+      />
+      
     </View>
   );
+
+  
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'center', 
     justifyContent: 'center',
   },
+
+  map:{
+    width: '100%',
+    height:'100%'
+    
+  }
 });
